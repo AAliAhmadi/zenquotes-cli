5e326bc65a5bd6fd233178c59180624305728c5b
AAliAhmadi <93519287+AAliAhmadi@users.noreply.github.com>
2025-05-09 20:15:47 +0330

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,44 +1,40 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import argparse
+import pyttsx3
 from quote_fetcher import QuoteFetcher
+from colorama import Fore, Style, init
 
-# Initialize the QuoteFetcher class
-fetcher = QuoteFetcher()
+init(autoreset=True)
 
 def main():
-    # Setup argument parsing
-    parser = argparse.ArgumentParser(description="Fetch inspirational quotes from ZenQuotes")
-    
-    parser.add_argument('--random', action='store_true', help="Get a random quote")
-    parser.add_argument('--today', action='store_true', help="Get today's quote")
-    parser.add_argument('--author', type=str, help="Search quotes by a specific author")
-    parser.add_argument('--list-authors', action='store_true', help="List popular authors")
-
+    parser = argparse.ArgumentParser(description="ZenQuotes CLI - Get inspired quotes from the terminal!")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--random", action="store_true", help="Fetch a random quote")
+    group.add_argument("--today", action="store_true", help="Fetch today's quote")
+    group.add_argument("--author", type=str, help="Fetch quote by a specific author")
+    parser.add_argument("--speak", action="store_true", help="Read the quote aloud")
+    parser.add_argument("--list-authors", action="store_true", help="List popular authors")
     args = parser.parse_args()
+
+    fetcher = QuoteFetcher()
+
+    if args.list_authors:
+        fetcher.list_authors()
+        return
 
     if args.random:
         fetcher.get_random()
-        print(fetcher.quote)
-        print(fetcher.author)
     elif args.today:
         fetcher.get_today()
-        print(fetcher.quote)
-        print(fetcher.author)
     elif args.author:
-        fetcher.search_author(args.author)
-        print(fetcher.quote)
-        print(fetcher.author)
-    elif args.list_authors:
-        print("Popular authors to choose from:")
-        print("Albert Einstein, Maya Angelou, Oscar Wilde, Confucius, Mark Twain, Rumi, Steve Jobs, Buddha, Socrates, Nelson Mandela")
-    else:
-        print("Please choose one of the options (--random, --today, --author AUTHOR, --list-authors)")
+        fetcher.get_by_author(args.author)
+
+    fetcher.display_quote()
+
+    if args.speak:
+        engine = pyttsx3.init()
+        engine.say(fetcher._quote)
+        engine.say(f"by {fetcher._author}")
+        engine.runAndWait()
 
 if __name__ == "__main__":
     main()
-
